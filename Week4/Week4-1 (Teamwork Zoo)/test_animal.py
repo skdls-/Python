89065d8b8af974268da3daef81ef27527341d385
Симеон Ролев <skdls@abv.bg>
2014-11-03 19:31:41 +0200

    Animal Class and Tests

---
+++ Week4/Week4-1 (Teamwork Zoo)/test_animal.py
@@ -0,0 +1,31 @@
+import unittest
+from animal import Animal
+
+class TestAnimal(unittest.TestCase):
+
+	def setUp(self):
+
+		self.my_animal = Animal("cat", 2, "Cat", "female", 3, 10)
+
+	def test_init(self):
+
+		self.assertEqual(self.my_animal.species, "cat")
+		self.assertEqual(self.my_animal.age, 2)
+		self.assertEqual(self.my_animal.name, "Cat")
+		self.assertEqual(self.my_animal.gender, "female")
+		self.assertEqual(self.my_animal.weight, 3)
+		self.assertEqual(self.my_animal.life_expactancy, 10)
+
+	def test_grow(self):
+
+		self.my_animal.grow(1)
+		self.assertEqual(self.my_animal.age, 3)
+
+	def test_eat(self):
+
+		self.my_animal.eat(3)
+		self.assertEqual(self.my_animal.weight, 6)
+
+if __name__ == '__main__':
+	unittest.main()
+
